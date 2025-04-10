375a6ace4103d964fe58042657965fa089610786
sla <sla>
2025-04-10 13:51:18 -0400

    ccccccccccccccccccccccc

--- produtos/urls.py
+++ produtos/urls.py
@@ -21,7 +21,5 @@ from django.urls import include, path
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.visualizarHome, name='home'),
-    path('loja/', include('produtos.urls')),
-] + static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path('', views.visualizarLoja, name='loja'),
+]
